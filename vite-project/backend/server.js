2890f32695c6d19707b7a14a39d4a4b17e4d8c94
FernandoAndrade10 <f.andrade.8.10@gmail.com>
2026-02-24 22:11:19 -0800

    Backend Deployment
    Cleaned up for Backend Deployment

--- vite-project/backend/server.js
+++ vite-project/backend/server.js
@@ -7,6 +7,14 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+if (process.env.VERCEL !== "1") {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;
+
 const pool = require('./db');
 
 const { sendRsvpConfirmationSms } = require("./twilio");
