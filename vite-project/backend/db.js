0702da958b2ba13df7de04b2ed604f32944558b4
FernandoAndrade10 <f.andrade.8.10@gmail.com>
2026-02-25 21:29:18 -0800

    update to db.js

--- vite-project/backend/db.js
+++ vite-project/backend/db.js
@@ -1,10 +1,16 @@
 /* eslint-env node */
+/* eslint-env node */
 const pg = require("pg");
 const { Pool } = pg;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: {
+        rejectUnauthorized: false,
+    },
+    max: 1,
+    idleTimeoutMillis: 0,
+    connectionTimeoutMillis: 10000,
 });
 
 module.exports = pool;
